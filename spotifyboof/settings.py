e064be893ee368b4b71dc87d40d81fcaee9cf2b5
derrickhua <derrickhua6@gmail.com>
2023-03-20 10:05:54 -0400

    Fix Secret Key

--- spotifyboof/settings.py
+++ spotifyboof/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-6*8)c)5fcf^nmm*pnx1%p$c2gh6w1xxev3a1g(8j1n=+67ufj6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
