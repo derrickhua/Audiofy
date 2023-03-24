65b48aeef8a9086b9677861a7a13acf245b13ed5
derrickhua <derrickhua6@gmail.com>
2023-03-24 11:59:14 -0400

    Put Up Django Auth and CRUD for Test model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Test, User
 
 # Register your models here.
+admin.site.register(Test)
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# for models associated in 1:M relationship with the user, add this: 
+# user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Test(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'test_id': self.id})
+    
