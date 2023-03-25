5b491251413c2e26e846f32119723fdd78b0952b
derrickhua <derrickhua6@gmail.com>
2023-03-25 15:21:40 -0400

    Made Playlists and Songs Model

--- main_app/views.py
+++ main_app/views.py
@@ -22,7 +22,6 @@ from .models import Playlist
 # BUCKET = 'needtosetup'
 
 
-
 def landing_page(request):
   return render(request, 'landing_page.html')
 
@@ -30,9 +29,6 @@ def landing_page(request):
 def playlist_index(request):
   return render(request, 'index.html')
 
-<<<<<<< HEAD
-=======
-
 class PlaylistCreate(LoginRequiredMixin, CreateView):
   model = Playlist
   fields = ['name']
@@ -49,7 +45,6 @@ class PlaylistDelete(LoginRequiredMixin, DeleteView):
   model = Playlist
   success_url = '/playlists'
 
->>>>>>> main
 @login_required
 def playlist_detail(request, test_id):
   playlists = Playlist.objects.get(id=test_id)
@@ -81,53 +76,6 @@ def signup(request):
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
-class PlaylistCreate(LoginRequiredMixin, CreateView):
-  model = Playlist
-  fields = ['name']
-  # This will assign a future TestModel to the currently logged in user
-  def form_valid(self, form):
-    form.instance.user = self.request.user  
-    return super().form_valid(form)
 
-class PlaylistUpdate(LoginRequiredMixin, UpdateView):
-  model = Playlist
-  fields = ['name']
 
-class PlaylistDelete(LoginRequiredMixin, DeleteView):
-  model = Playlist
-  success_url = '/playlists'
 
-<<<<<<< HEAD
-
-=======
-@login_required
-def playlist_detail(request, test_id):
-  playlists = Playlist.objects.get(id=test_id)
-  return render(request, 'playlist/detail.html', {
-    'playlists': playlists, 
-  })
-
-@login_required
-def playlist_index(request):
-  playlists = Playlist.objects.filter(user=request.user)
-  return render(request, 'playlist/index.html', { 'playlist': playlists })
-
-def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      # This will add the user to the database
-      user = form.save()
-      # This is how we log a user in via code
-      login(request, user)
-      return redirect('index')
-    else:
-      error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
->>>>>>> main
